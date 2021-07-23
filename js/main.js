c52a78c30a54278f354458cab3d4f358170b8780
Darasimi Ajewole <darasimi@formpl.us>
2021-07-23 21:12:30 +0100

    Pushed Some bugfixes to code

--- js/main.js
+++ js/main.js
@@ -1,115 +1,106 @@
+'use strict'
+
 class Calculator {
-    constructor() {
-        this.banNum = 5000 ;      //amount of bananas from farm
-        this.distance = 5000 ;     // distance between farm and market
-        this.banCost = 5;            //cost of feeding one camel per Km
-    }
-    
-    //updates the front end slider display value
-    updateDisplay (event) {
-        var sliderDisplay = event.target.nextElementSibling;
-        sliderDisplay.textContent = event.target.value;
-        
-    }
-    
-    //updates attributes of class
-    updateData(event) {
-        var input = event.target.getAttribute("name");
-        var value = event.target.value;
-        
-        switch(input) {
-            case "banNum":
-                this.banNum = value;
-                break;
-            
-            case "distance":
-                this.distance = value;
-                break;
-            
-            case "banCost":
-                this.banCost = value;
-                break
-        }
-        
-    }
-    
-    //compute maximum amount of banana to arrive at  Market
-    computeNetBan() { 
-        
-        var movedKm = 0; //distance covered
-        var expCamels = this.banNum/1000;    //number of camels          //expected camels  to carry banana from farm
-        var x = this.banNum;
-        var y = this.distance;
-        var k = this.banCost
 
-        var message;
-        var tip;
+	constructor() {
+		this.banNum = 5000;      // amount of bananas from farm
+		this.distance = 5000;    // distance between farm and market
+		this.banCost = 1;         // cost of feeding one camel per Km
+	}
 
-        while (true) {
-            
-            var dn = 1000/(expCamels * k); //distance moved at 1,000 bananas consumed
-            movedKm += dn;
-            
-            //distance moved greater than y; distance between farm and market
-            if (movedKm > y) { 
-                var excKm =  movedKm - y; //excess kilometer
-                var costExc = excKm * k * expCamels; //cost of excess kilometer
-                x += costExc; 
-                break;
-            }
-            
-            expCamels -= 1; //reducing camel number to reduce cost
-            x -= 1000; //reducing banana number
-            
-            //if one thousand banana left
-            if (x == 1000) {
-                var remKm = y - movedKm;
-                if (remKm > x) { 
-                    message = "No Banana left to sell";
-                    tip = "reduce banana cost per KM or distance to Market";
-                }
-                else { 
-                    x -= remKm;
-                    message = `You have ${Math.round(x)} bananas to sell at the Market`;
-                    tip = "";
-                }
-                var _dict = {
-                    "message": message,
-                    "tip": tip
-                }
-                return _dict
-            }
-        }
-        
-        message = `You have ${Math.round(x)} bananas to sell at the Market`;
-        tip = "";
+	// updates the front end slider display value
+	updateDisplay(event) {
+		var sliderDisplay = event.target.nextElementSibling;
+		sliderDisplay.textContent = event.target.value;
+	}
 
-        var _dict = {
-            "message": message,
-            "tip": tip
-        }
-        return _dict
-    }
+	// updates attributes of class
+	updateData(event) {
+		const input = event.target.getAttribute("name");
+		const value = event.target.value;
+
+		switch (input) {
+			case "banNum":
+				this.banNum = value;
+				break;
+
+			case "distance":
+				this.distance = value;
+				break;
+
+			case "banCost":
+				this.banCost = value;
+				break
+		}
+
+	}
+
+	// compute maximum amount of banana to arrive at  Market
+	computeNetBan() {
+
+		let movedKm = 0; //	distance covered
+		let expCamels = this.banNum / 1000;	//	number of camels expected to carry banana from farm
+		let distance = this.distance;
+		let banNum = this.banNum;
+		let banCost = this.banCost
+
+		let message;
+		let tip;
+
+		if (distance => banNum) {
+			message = "No Banana left to sell";
+			tip = "reduce banana cost per KM or distance to Market";
+
+			return { message, tip }
+		}
+
+		while (true) {
+
+			banNum -= 1000; // reducing banana number by amount to be consumed;
+			movedKm += 1000 / (expCamels * banCost); // distance moved at 1,000 bananas consumed;
+			distance -= movedKm;
+
+			if (distance <= 0) {	// journey completed
+				const excKm = -1 * distance; //	surplus kilometer
+				const costExc = excKm * banCost * expCamels; //	cost of surplus kilometer
+				banNum += costExc;
+				message = `You have ${Math.round(banNum)} bananas to sell at the Market`;
+				tip = "";
+				break
+			}
+	
+			if (banNum === 0) {	// banana is exhausted before destination
+				message = "No Banana left to sell";
+				tip = "reduce banana cost per KM or distance to Market";
+				break
+			}
+
+			expCamels -= 1; //	reducing camel number to reduce cost
+		}
+
+		return { message, tip }
+
+	}
 }
 
-function compute(event,calculator) {
-    if (event.target.nodeName === "INPUT") {
-        calculator.updateDisplay(event);
-        calculator.updateData(event);
-        
-        var netBanValue = calculator.computeNetBan();
-        var netBan = document.querySelector(".netBan");
-        var tip = document.querySelector(".tip");
-        netBan.textContent = netBanValue.message;
-        netBan.style.border = "3px solid goldenrod"
-        tip.textContent = `*tip: ${netBanValue.tip}`;
-    }
+function compute(event, calculator) {
+	if (event.target.nodeName === "INPUT") {
+		calculator.updateDisplay(event);
+		calculator.updateData(event);
+
+		var netBanValue = calculator.computeNetBan();
+		var netBan = document.querySelector(".netBan");
+		var tip = document.querySelector(".tip");
+		netBan.textContent = netBanValue.message;
+		netBan.style.border = "3px solid goldenrod"
+		tip.textContent = `*tip: ${netBanValue.tip}`;
+	}
 }
 
 function main() {
-	var calculator = new Calculator(); 
+	var calculator = new Calculator();
 	var sliders = document.querySelector(".sliders");
-	sliders.addEventListener("change", (event)=> {compute(event,calculator)});	
-	}
+	sliders.addEventListener("change", (event) => compute(event, calculator) );
+}
 
 main()
